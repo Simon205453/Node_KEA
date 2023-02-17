e0e327252258e000266c00fca95b31d1d2a291bd
Simon <simonffsss@gmail.com>
2023-02-17 11:49:45 +0100

    this is tanks

--- 03._Plain_HTML_Server/app.js
+++ 03._Plain_HTML_Server/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-        res.sendFile(__dirname + "/frontpage.html");
+        res.sendFile(__dirname + "/public/frontpage/frontpage.html");
 })
+
+app.get("/tanks", (req, res) => {
+        res.sendFile(__dirname + "/public/tanks/tanks.html");
+});
+
 
 
 const PORT = 8080;
